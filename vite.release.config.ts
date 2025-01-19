966cf1d3f8c075d255e97f4e4b4476e64911fb59
Randyhuls <randyhuls@hotmail.com>
2025-01-19 16:32:41 +0100

    Version 0.1.2

--- vite.release.config.ts
+++ vite.release.config.ts
@@ -1,5 +1,6 @@
 // Vite plugins
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts'
 
@@ -12,7 +13,7 @@ import { createPackageJSON } from './compiler/build.utils';
 
 const BUILD_PATH = './artifacts';
 const BUNDLE_NAME = 'bundle'
-const VERSION_NUMBER = '0.1.1'; // [!] Increment before publish new build
+const VERSION_NUMBER = '0.1.2'; // [!] Increment before publish new build
 const IS_PRIVATE_PACKAGE = true; // [!] Set to false to allow public access on the NPM registry
 
 const srcPackageJSON: Record<string, unknown> = JSON.parse(readFileSync('./package.json', 'utf-8').toString());
@@ -53,6 +54,11 @@ export default defineConfig({
         })
       }     
     },
+    viteStaticCopy({
+      targets: [
+        { src: './README.md', dest: outputDir },
+      ],
+    }),
     dts({ 
       rollupTypes: true,
       tsconfigPath: './tsconfig.app.json',
