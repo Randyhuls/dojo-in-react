bbc6f026ba4e8156b49a7a5a4547fde39a176424
Randyhuls <randyhuls@hotmail.com>
2025-01-20 09:46:39 +0100

    Expanded examples of Dojo widgets in React

--- src/components/dojo/index.ts
+++ src/components/dojo/index.ts
@@ -1,4 +1,5 @@
 import { loadDojoWidget } from '../../utils/dojo-in-react.utils.js';
 
 // Reexport AMD modules as ESM modules
-export const Button = loadDojoWidget('dojo-widgets/Button/Button.dojo.js');
+export const Button = loadDojoWidget('dojo-widgets/Button/Button.dojo');
+export const Select = loadDojoWidget('dijit/form/Select');
--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,53 @@
 import { useEffect, useState } from 'react';
 
 // Components
-import { Button } from './components/dojo';
+import { Button, Select } from './components/dojo';
 import withReact from './components/hocs/withReact';
 
-const ReactInDojoButton = withReact<{ label: string }>(await Button);
+const DojoInReactButton = withReact<{ label: string }>(await Button);
+const SelectInReact = withReact(await Select);
 
 export const Main = () => {
   const [widgetState, setWidgetState] = useState<Record<string, unknown>>({});
   const [label, setLabel] = useState('Dojo-in-React Button');
   const [hideDojoButton, setHideDojoButton] = useState(false);
-  
+
   useEffect(() => {
     console.log('Dojo widget state in React:', widgetState)
   }, [widgetState])
 
   return (
-    <div 
-      style={{
-        display: 'flex',
-        gap: '10px'
-      }}
-    >
-      {!hideDojoButton && (
-        <ReactInDojoButton 
-          label={label}   
-          subscribedProps={['label']}
-          onPropChange={(prop) => {
-            setWidgetState({ ...widgetState, ...prop })
-          }}
-        />
-      )}
-      <button onClick={setLabel.bind(null, 'Still a Dojo button!')}>Change Dojo Button label</button>
-      <button onClick={setHideDojoButton.bind(null, !hideDojoButton)}>{!hideDojoButton ? 'Hide' : 'Show'} Dojo Button</button>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+    }}>
+      <div 
+        style={{
+          display: 'flex',
+          gap: '10px'
+        }}
+      >
+        {!hideDojoButton && (
+          <DojoInReactButton 
+            label={label}   
+            subscribedProps={['label']}
+            onPropChange={(prop) => {
+              setWidgetState({ ...widgetState, ...prop })
+            }}
+          />
+        )}
+        <button onClick={setLabel.bind(null, 'Still a Dojo button!')}>Change Dojo Button label</button>
+        <button onClick={setHideDojoButton.bind(null, !hideDojoButton)}>{!hideDojoButton ? 'Hide' : 'Show'} Dojo Button</button>
+      </div>
+
+      <SelectInReact  
+        name="Select dropdown in React"
+        className="mySelectInReactClass"
+        options={[
+          { label: "Dojo", value: "dojo" },
+          { label: "React", value: "react", selected: true },
+        ]}
+      />
     </div>
   )
 };
